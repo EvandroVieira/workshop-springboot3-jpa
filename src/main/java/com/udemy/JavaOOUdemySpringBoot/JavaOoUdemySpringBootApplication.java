ff5b245dc7f841c713b6b6d5a08ae98e2e7c2e32
EvandroVieira <evandro.vieira@elo.com.br>
2024-12-27 17:59:38 -0300

    Project created

---
+++ src/main/java/com/udemy/JavaOOUdemySpringBoot/JavaOoUdemySpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.JavaOOUdemySpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaOoUdemySpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaOoUdemySpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/udemy/JavaOOUdemySpringBoot/JavaOoUdemySpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.JavaOOUdemySpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaOoUdemySpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
