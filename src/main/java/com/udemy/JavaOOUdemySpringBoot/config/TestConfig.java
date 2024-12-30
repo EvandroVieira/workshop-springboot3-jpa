96329aaa57cee8668cd496bd4c7f93f0d794aa98
EvandroVieira <evandro.vieira@elo.com.br>
2024-12-30 16:01:23 -0300

    OrderStatus enum

--- src/main/java/com/udemy/JavaOOUdemySpringBoot/config/TestConfig.java
+++ src/main/java/com/udemy/JavaOOUdemySpringBoot/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.udemy.JavaOOUdemySpringBoot.config;
 
 import com.udemy.JavaOOUdemySpringBoot.entities.Order;
 import com.udemy.JavaOOUdemySpringBoot.entities.User;
+import com.udemy.JavaOOUdemySpringBoot.entities.enums.OrderStatus;
 import com.udemy.JavaOOUdemySpringBoot.repositories.OrderRepository;
 import com.udemy.JavaOOUdemySpringBoot.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,9 +28,9 @@ public class TestConfig implements CommandLineRunner {
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "1233456");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "1233456");
 
-        Order o1 = new Order(null, Instant.parse("2024-11-20T19:53:07Z"), u1);
-        Order o2 = new Order(null, Instant.parse("2024-12-21T03:42:10Z"), u2);
-        Order o3 = new Order(null, Instant.parse("2024-12-22T15:21:22Z"), u1);
+        Order o1 = new Order(null, Instant.parse("2024-11-20T19:53:07Z"), OrderStatus.PAID, u1);
+        Order o2 = new Order(null, Instant.parse("2024-12-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
+        Order o3 = new Order(null, Instant.parse("2024-12-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
 
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
